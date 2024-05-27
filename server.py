6402161c5a3f2e0e7d9cd62a521b4f85a447747e
thebosbos <boussaadam7@gmail.com>
2024-05-27 09:56:38 +0100

    first-one

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+from database import *
+
+app = Flask(__name__)
+
+@app.route('/get_response')
+def execute_python_file():
+    # Execute the Python file and capture its output
+    return main()
+
+
+@app.route('/')
+def hello():
+
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Get port from environment variable or use default 5000
+    app.run(debug=True, host='0.0.0.0', port=port)  # Explicitly set host and port
